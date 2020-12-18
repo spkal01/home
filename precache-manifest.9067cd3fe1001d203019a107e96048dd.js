d843bce844ea6628a2149e3ae7ce711646f3bb4e
spkal01 <kalligeross@gmail.com>
2020-12-18 23:28:48 +0200

    Updates

---
+++ precache-manifest.9067cd3fe1001d203019a107e96048dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a422881e8f3bd40bd54f3e30ccaad0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "839a4bce26ee82e0722b",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "57869e7269f237aa8835",
+    "url": "/static/js/2.c85af4d1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c85af4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839a4bce26ee82e0722b",
+    "url": "/static/js/main.301167f6.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "61a6826d0885cf0c676a67c88bc2e1b4",
+    "url": "/static/media/spkal.61a6826d.jpg"
+  }
+]);
